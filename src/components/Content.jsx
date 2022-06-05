ed814f3a90b9f1ca8b4c4cb905927bab9f5d6458
Michael Pimonov <off_mi4ka@vk.com>
2022-06-05 13:29:50 +0700

    CreateSocialLink

---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+const Content = () => {
+    return (
+        <div className='content'>
+            <div><img className='bg-profile' src="https://mirpozitiva.ru/wp-content/uploads/2019/11/1477469618_ozero_v_lesu.jpg" alt=""/></div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return(<header className='header'>
+        <img src="https://i.pinimg.com/originals/22/22/42/222242f7e26848e776503c565175673a.png" alt=""/>
+    </header>
+    )
+
+}
+export default Header;
